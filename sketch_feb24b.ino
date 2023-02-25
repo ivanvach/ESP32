248904257925c6c2c72b91be8875abd1a96806d4
ivanvach <43918172+ivanvach@users.noreply.github.com>
2023-02-25 15:11:26 -0800

    Update sketch_feb24b.ino

--- sketch_feb24b.ino
+++ sketch_feb24b.ino
@@ -1,7 +1,10 @@
+
 // this code is working
 // if WiFi is not connected or if there are no WiFi credentials in the EEPROM memory
 // than the code starts AP with name ESP32AP and password: password
 // at IP address 192.168.4.1 one can enter WiFi credentials
+// Also it allows for entering MQTT credentials from the web form 
+// it is public onGitHub under the name of ESP32
 
 
 
@@ -14,6 +17,13 @@ const int SSID_SIZE = 32;
 const int PASSWORD_SIZE = 32;
 const char* ACCESS_POINT_NAME = "ESP32AP";
 const char* ACCESS_POINT_PASSWORD = "password";
+const int MQTT_SERVER_SIZE = 64;
+const int MQTT_PORT_SIZE = 6; // Max value is 65535, which is 5 characters
+const int MQTT_USERNAME_SIZE = 32;
+const int MQTT_PASSWORD_SIZE = 32;
+#define DEFAULT_MQTT_PORT 1883
+
+
 
 AsyncWebServer server(80);
 
@@ -29,7 +39,11 @@ void setup() {
 void checkWiFiCredentials() {
   String ssid = "";
   String password = "";
-
+  String mqttServer = "";
+  String mqttPort = "";
+  String mqttUsername = "";
+  String mqttPassword = "";
+  // Read WiFi credentials from EEPROM
   for (int i = 0; i < SSID_SIZE; i++) {
     ssid += char(EEPROM.read(i));
   }
@@ -38,42 +52,69 @@ void checkWiFiCredentials() {
     password += char(EEPROM.read(i + SSID_SIZE));
   }
 
-  if (ssid != "" && password != "") {
-    Serial.print("Saved WiFi credentials found. SSID: ");
-    Serial.print(ssid);
-    Serial.print(", Password: ");
-    Serial.println(password);
-
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid.c_str(), password.c_str());
-    Serial.println("Connecting to WiFi...");
-
-    // Wait for WiFi to connect, or timeout after 30 seconds
-    int wifiTimeout = 30000;
-    unsigned long start = millis();
-    while (WiFi.status() != WL_CONNECTED && millis() - start < wifiTimeout) {
-      Serial.print(".");
-      delay(1000);
-    }
-
-    // If WiFi connection was successful, print out the IP address assigned to the device
-    if (WiFi.status() == WL_CONNECTED) {
-      Serial.println("");
-      Serial.print("WiFi connected. IP address: ");
-      Serial.println(WiFi.localIP());
-    }
-    // If WiFi connection was not successful, start the access point
-    else {
-      Serial.println("");
-      Serial.println("WiFi connection failed. Starting access point...");
-      startAccessPoint();
-    }
+  // Read MQTT credentials from EEPROM
+  for (int i = 0; i < MQTT_SERVER_SIZE; i++) {
+    mqttServer += char(EEPROM.read(i + SSID_SIZE + PASSWORD_SIZE));
   }
-  else {
-    Serial.println("No saved WiFi credentials found. Starting access point...");
+
+  for (int i = 0; i < MQTT_PORT_SIZE; i++) {
+    mqttPort += char(EEPROM.read(i + SSID_SIZE + PASSWORD_SIZE + MQTT_SERVER_SIZE));
+  }
+
+  for (int i = 0; i < MQTT_USERNAME_SIZE; i++) {
+    mqttUsername += char(EEPROM.read(i + SSID_SIZE + PASSWORD_SIZE + MQTT_SERVER_SIZE + MQTT_PORT_SIZE));
+  }
+
+  for (int i = 0; i < MQTT_PASSWORD_SIZE; i++) {
+    mqttPassword += char(EEPROM.read(i + SSID_SIZE + PASSWORD_SIZE + MQTT_SERVER_SIZE + MQTT_PORT_SIZE + MQTT_USERNAME_SIZE));
+  }
+
+  if (ssid != "" && password != "") {
+    Serial.println("Saved WiFi credentials found. Connecting to WiFi...");
+    Serial.print("SSID: ");
+    Serial.println(ssid);
+    Serial.print("WiFi Password: ");
+    Serial.println(password);
+    WiFi.begin(ssid.c_str(), password.c_str());
+    delay(40000); // Wait 40 seconds for connection to be established
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("WiFi connected.");
+    } else {
+      Serial.println("Failed to connect to saved WiFi network. Clearing saved credentials...");
+      clearCredentials();
+    }
+  } else {
+    Serial.println("No saved WiFi credentials found.");
+  }
+
+Serial.print("SSID: ");
+Serial.println(ssid);
+Serial.print("WiFi Password: ");
+Serial.println(password);
+
+Serial.print("mqttServer1: ");
+Serial.println(mqttServer);
+Serial.print("mqttPort1: ");
+Serial.println(mqttPort);
+Serial.print("mqttUsername1: ");
+Serial.println(mqttUsername);
+Serial.print("mqttPassword1: ");
+Serial.println(mqttPassword);
+  
+  // Check if MQTT credentials are present
+  if (mqttServer != "" && mqttPort != "" && mqttUsername != "" && mqttPassword != "") {
+    Serial.println("Saved MQTT credentials found.");
+    // Connect to MQTT broker
+  } else {
+    Serial.println("No saved MQTT credentials found.");
+  }
+
+  if (ssid == "" || password == "") {
+    Serial.println("Starting access point...");
     startAccessPoint();
   }
 }
+
 
 
 
@@ -92,46 +133,98 @@ void startAccessPoint() {
 }
 
 void handleRoot(AsyncWebServerRequest *request) {
-  String ssid = WiFi.SSID();
-  String html = "<html><body>";
-  html += "<h1>WiFi Credentials</h1>";
-  html += "<form action=\"/save\">";
-  html += "<label for=\"ssid\">SSID:</label><br>";
-  html += "<input type=\"text\" id=\"ssid\" name=\"ssid\" value=\"" + ssid + "\"><br>";
-  html += "<label for=\"password\">Password:</label><br>";
-  html += "<input type=\"password\" id=\"password\" name=\"password\"><br><br>";
+  String html = "<form action=\"/save\">";
+  html += "<label>WiFi SSID:</label><input type=\"text\" name=\"ssid\"><br>";
+  html += "<label>WiFi Password:</label><input type=\"password\" name=\"password\"><br>";
+  html += "<label>MQTT Broker:</label><input type=\"text\" name=\"mqttServer\"><br>";
+  html += "<label>MQTT Port:</label><input type=\"text\" name=\"mqttPort\"><br>";
+  html += "<label>MQTT Username:</label><input type=\"text\" name=\"mqttUsername\"><br>";
+  html += "<label>MQTT Password:</label><input type=\"password\" name=\"mqttPassword\"><br>";
   html += "<input type=\"submit\" value=\"Save\">";
   html += "</form>";
-  html += "</body></html>";
   request->send(200, "text/html", html);
 }
 
 
-void handleSave(AsyncWebServerRequest *request) {
-  String ssid = request->arg("ssid");
-  String password = request->arg("password");
-  request->redirect("/");
-  
-  Serial.println("Saving WiFi credentials...");
-  
-  // Clear the EEPROM memory first
-  for (int i = 0; i < EEPROM_SIZE; i++) {
-    EEPROM.write(i, 0);
-  }
-  EEPROM.commit();
 
-  // Write the new SSID and password to the EEPROM
+void handleSave(AsyncWebServerRequest *request) {
+ 
+  Serial.print("Saving started....");
+  
+  String ssid = request->getParam("ssid")->value();
+  String password = request->getParam("password")->value();
+  String mqttServer = request->getParam("mqttServer")->value();
+  String mqttPort = request->getParam("mqttPort")->value();
+  String mqttUsername = request->getParam("mqttUsername")->value();
+  String mqttPassword = request->getParam("mqttPassword")->value();
+
+
+  request->send(303, "text/plain", "Credentials saved.");
+  
+Serial.print("mqttServer2: ");
+Serial.println(mqttServer);
+Serial.print("mqttPort2: ");
+Serial.println(mqttPort);
+Serial.print("mqttUsername2: ");
+Serial.println(mqttUsername);
+Serial.print("mqttPassword2: ");
+Serial.println(mqttPassword);
+
+  if (ssid.length() == 0 || password.length() == 0) {
+    request->send(400, "text/plain", "SSID and password are required");
+    return;
+  }
+
+  Serial.println("Saving WiFi credentials...");
+
+// Clear previous wifi credentials
+
+clearCredentials();
+  
   for (int i = 0; i < ssid.length(); i++) {
     EEPROM.write(i, ssid[i]);
   }
+
   for (int i = 0; i < password.length(); i++) {
     EEPROM.write(i + SSID_SIZE, password[i]);
   }
+
+// clear previous mqtt values
+
+for (int i = SSID_SIZE + PASSWORD_SIZE; i < SSID_SIZE + PASSWORD_SIZE + MQTT_SERVER_SIZE + MQTT_PORT_SIZE + MQTT_USERNAME_SIZE + MQTT_PASSWORD_SIZE ; i++) {
+    EEPROM.write(i, 0);}
+ 
+  
+  
+  for (int i = 0; i < mqttServer.length(); i++) {
+    EEPROM.write(i + SSID_SIZE + PASSWORD_SIZE, mqttServer[i]);
+  }
+
+  String mqttPortString = String(DEFAULT_MQTT_PORT);
+  if (mqttPort.length() > 0) {
+    mqttPortString = mqttPort;
+  }
+
+  int portSize = mqttPortString.length();
+  for (int i = 0; i < portSize; i++) {
+    EEPROM.write(i + SSID_SIZE + PASSWORD_SIZE + MQTT_SERVER_SIZE, mqttPortString[i]);
+  }
+
+  for (int i = 0; i < mqttUsername.length(); i++) {
+    EEPROM.write(i + SSID_SIZE + PASSWORD_SIZE + MQTT_SERVER_SIZE + MQTT_PORT_SIZE, mqttUsername[i]);
+  }
+
+  for (int i = 0; i < mqttPassword.length(); i++) {
+    EEPROM.write(i + SSID_SIZE + PASSWORD_SIZE + MQTT_SERVER_SIZE + MQTT_PORT_SIZE + MQTT_USERNAME_SIZE, mqttPassword[i]);
+  }
+
   EEPROM.commit();
-  Serial.println("Saved WiFi credentials. Restarting...");
+
+  Serial.println("Saved WiFi and MQTT credentials. Restarting...");
   delay(2000);
   ESP.restart();
 }
+
 
 
 
